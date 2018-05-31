bd9a1023428411d9054df6456bd11062341a75fa
Bobby Pinard <bobby.pinard@gmail.com>
2018-05-30 19:06:19 -0500

    Removed placeholder code.

--- todo-app/src/Card/Card.jsx
+++ todo-app/src/Card/Card.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
-import { Button, Panel } from 'react-bootstrap'
-import './Card.css'
 
 export default class Card extends Component {
   render() {
     return (
       <div>
-        <Panel bsStyle="success">
-          <Panel.Heading>Head</Panel.Heading>
-          <Panel.Body>Body</Panel.Body>
-        </Panel>
-        <Button bsStyle="success">Success</Button>
+        <h1>Hi.</h1>
       </div>
     )
   }
