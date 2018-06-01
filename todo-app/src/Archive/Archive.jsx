60909d55dbd02606361caf14247399588be87661
Bobby Pinard <bobby.pinard@gmail.com>
2018-05-31 21:43:20 -0500

    Aded Archive component for archived todos.

---
+++ todo-app/src/Archive/Archive.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Archive.css'
+
+export default class Archive extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
