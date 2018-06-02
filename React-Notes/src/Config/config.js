bccae9e93d424aac4d2510d442c5203352b162c5
Bobby Pinard <bobby.pinard@gmail.com>
2018-06-02 17:41:22 -0500

    Updated project title formatting.

---
+++ React-Notes/src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyDHKJ46RL4XgAJJE8CMey2HbJhcCxwHVms",
+  authDomain: "reactnotes-455b2.firebaseapp.com",
+  databaseURL: "https://reactnotes-455b2.firebaseio.com",
+  projectId: "reactnotes-455b2",
+  storageBucket: "reactnotes-455b2.appspot.com",
+  messagingSenderId: "924148132091"
+};
